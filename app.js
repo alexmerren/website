2bf3476bc9ec5631e5591b5279040afc36263bf1
alexmerren <alexandermerren@gmail.com>
2021-04-06 14:53:59 +0100

    Updates

--- app.js
+++ app.js
@@ -1,22 +1,30 @@
 const express = require('express');
 const url = require('url');
 const app = express();
-const port = 8080;
+const PORT = 8080;
 
-// Set the images/stylesheets to be in /static.
-app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/public'));
+// Set the images/stylesheets to be in /static
+app.use(express.static(__dirname + '/static'));
+
+// Tell the server to render ejs files
 app.set('trust proxy', true);
+app.set('view engine', 'ejs');
 
-// Opens up the server to port 8080.
-app.listen(port, function () {
-    console.log('An app listening on port 8080');
+// Opens up the server to the port specified above 
+app.listen(PORT, function () {
+    console.log(`An app listening on port ${PORT}`);
 });
 
-// Whenever a request is made to the server, it 
-// responds with trying to find that url + /index.
-app.use('/', function(req, res) {
-    var reqUrl = url.parse(req.url, true).pathname;
-    var pathname = '.' + reqUrl + '/index';
-    res.render(pathname);
+// Render the homepage upon visiting
+app.get('/', function(req, res) {
+    res.render('index') 
 });
+
+// When a request is made to 'www.website.com/page' it redirects to page/index 
+//app.use('/', function(req, res) {
+//    var reqUrl = url.parse(req.url, true).pathname;
+//    var pathname = '.' + reqUrl + '/index';
+//    res.render(pathname);
+//});
+
+
