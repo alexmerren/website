a3cde5e6b89fe66a5e666da149450d2259ac69f0
Alex Merren <alexandermerren@gmail.com>
2021-04-08 11:54:05 +0100

    updates

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,61 +1,90 @@
-// The 'main' function when the document is loaded
-$(document).ready(function() {
 
-    // Check if the users prefers dark or light theme to start
+// Smooth scrolling function
+$(document).ready( function() {
     if (
-        localStorage.getItem('color-mode') === 'dark' || (window.matchMedia('(prefers-color-scheme: dark)').matches && !localStorage.getItem('color-mode'))
+        localStorage.getItem("color-mode") === "dark" ||
+        (window.matchMedia("(prefers-color-scheme: dark)").matches &&
+            !localStorage.getItem("color-mode"))
     ) {
-        $('html').attr('color-mode', 'dark');
+        $("html").attr("color-mode", "dark");
     }
 
-    // Activate the onScroll() function for the document
-    $(document).on('scroll', onScroll)
+    $(document).on("scroll", onScroll);
 
-    // Smooth scrolling function
-    $('a[href^="#"]').on('click', function (e) {
+    $('a[href^="#"]').on("click", function (e) {
         e.preventDefault();
-        $(document).off('scroll');
+        $(document).off("scroll");
 
-        $('a').each(function() {
-            $(this).removeClass('active');
+        $("a").each(function () {
+            $(this).removeClass("active");
         });
-        $(this).addClass('active');
+        $(this).addClass("active");
 
         var target = this.hash;
         $target = $(target);
 
-        $('html, body').stop().animate({
-            'scrollTop': $target.offset().top+2
-        }, 300, function() {
-            window.location.has = target;
-            $(document).on('scroll', onScroll);
-        });
+        $("html, body").stop().animate(
+            { scrollTop: $target.offset().top + 2 },
+            300,
+            function () {
+                window.location.has = target;
+                $(document).on("scroll", onScroll);
+            }
+        );
     });
+    
 });
 
-// Function to change the highlighted section in navbar 
+// Function to change the highlighted section in navbar
 function onScroll() {
-    var scrollPos = $(document).scrollTop();
-    $('.nav a').each(function () {
+    var scrollPos = $(document).scrollTop() + ($(window).height())/2;
+    $(".nav a").slice(1, 5).each(function () {
         var currentLink = $(this);
-        var refElement = $(currentLink.attr('href'));
-        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
-            $('.nav a').removeClass('active');
-            currentLink.addClass('active');
+        var refElement = $(currentLink.attr("href"));
+        if (scrollPos >= refElement.position().top) {
+            $(".nav a").removeClass("active");
+            currentLink.addClass("active");
         } else {
-            currentLink.removeClass('active');
+            currentLink.removeClass("active");
         }
     });
 }
 
 // Function to toggle dark mode
 function toggleTheme() {
-    if ($('html').attr('color-mode') == 'light') {
-        $('html').attr('color-mode', 'dark');
-        localStorage.setItem("color-mode", "dark");
-    } else if ($('html').attr('color-mode') == 'dark') {
-        $('html').attr('color-mode', 'light');
-        localStorage.setItem("color-mode", "light");
+    if ($("html").attr("color-mode") == "light") {
+        $("html").attr("color-mode", "dark");
+    } else {
+        $("html").attr("color-mode", "light");
     }
+    localStorage.setItem("color-mode", $("html").attr("color-mode"));
 }
 
+$(document).ready( function() {
+    let data = cleanRepoData();
+});
+
+function cleanRepoData() {
+    var dataURL = "https://api.github.com/users/alexmerren/repos"
+    let cleanedData = [];
+
+    $.getJSON(dataURL, function(data) {
+        console.log(data);
+        data.forEach(function(item) {
+            if (item.full_name == "alexmerren/alexmerren") {}
+            else {
+                let object = {
+                    'name' : item.name,
+                    'description' : item.description,
+                    'url' : item.url,
+                    'last_update' : item.pushed_at,
+                    'language' : item.language
+                };
+
+                cleanedData.push(object);
+            }
+        });
+    });
+
+    return cleanedData;
+}
