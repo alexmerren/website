c2e28447454b17ec32f9acf41c36d4d45c7896bd
Alex Merren <alexandermerren@gmail.com>
2021-04-13 12:44:03 +0100

    Latest update

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,6 +1,18 @@
+// Function to be executed when website is loaded
+$(document).ready(function () {
+    // Getting repos from Github when requested
+    // let data = cleanRepoData();
 
-// Smooth scrolling function
-$(document).ready( function() {
+    // Making the arrow bounce up and down at the bottom of the home div
+    setInterval(function() {
+        $("#title__down-arrow").show();
+        // setInterval(function() {
+        //     $("#down-arrow").effect("bounce", { times: 3, distance: 15 }, "slow");
+        // }, 1000);
+    }, 5000);
+
+
+    // Checking and setting color theme    
     if (
         localStorage.getItem("color-mode") === "dark" ||
         (window.matchMedia("(prefers-color-scheme: dark)").matches &&
@@ -9,9 +21,12 @@ $(document).ready( function() {
         $("html").attr("color-mode", "dark");
     }
 
+    // Whenever scrolling, use the onScroll function
     $(document).on("scroll", onScroll);
 
-    $('a[href^="#"]').on("click", function (e) {
+    // Whenever clicking on a link that points inside the web page,
+    // use this custom function.
+    $("a[href^='#']").on("click", function (e) {
         e.preventDefault();
         $(document).off("scroll");
 
@@ -23,31 +38,56 @@ $(document).ready( function() {
         var target = this.hash;
         $target = $(target);
 
-        $("html, body").stop().animate(
-            { scrollTop: $target.offset().top + 2 },
-            300,
-            function () {
+        $("html, body")
+            .stop()
+            .animate({ scrollTop: $target.offset().top }, 300, function () {
                 window.location.has = target;
                 $(document).on("scroll", onScroll);
-            }
-        );
+            });
     });
-    
 });
 
 // Function to change the highlighted section in navbar
 function onScroll() {
-    var scrollPos = $(document).scrollTop() + ($(window).height())/2;
-    $(".nav a").slice(1, 5).each(function () {
-        var currentLink = $(this);
-        var refElement = $(currentLink.attr("href"));
-        if (scrollPos >= refElement.position().top) {
-            $(".nav a").removeClass("active");
-            currentLink.addClass("active");
-        } else {
-            currentLink.removeClass("active");
-        }
+    var scrollPos = $(document).scrollTop() + $(window).height() / 2;
+
+    $(".nav a")
+        .slice(1, 5)
+        .each(function () {
+            var currentLink = $(this);
+            var refElement = $(currentLink.attr("href"));
+            if (scrollPos >= refElement.position().top) {
+                $(".nav a").removeClass("active");
+                currentLink.addClass("active");
+            } else {
+                currentLink.removeClass("active");
+            }
+        });
+}
+
+function cleanRepoData() {
+    var dataURL = "https://api.github.com/users/alexmerren/repos";
+    let cleanedData = [];
+
+    $.getJSON(dataURL, function (data) {
+        console.log(data);
+        data.forEach(function (item) {
+            if (item.full_name == "alexmerren/alexmerren") {
+            } else {
+                let object = {
+                    "name": item.name,
+                    "description": item.description,
+                    "url": item.url,
+                    "last_update": item.pushed_at,
+                    "language": item.language,
+                };
+
+                cleanedData.push(object);
+            }
+        });
     });
+
+    return cleanedData;
 }
 
 // Function to toggle dark mode
@@ -58,33 +98,4 @@ function toggleTheme() {
         $("html").attr("color-mode", "light");
     }
     localStorage.setItem("color-mode", $("html").attr("color-mode"));
-}
-
-$(document).ready( function() {
-    let data = cleanRepoData();
-});
-
-function cleanRepoData() {
-    var dataURL = "https://api.github.com/users/alexmerren/repos"
-    let cleanedData = [];
-
-    $.getJSON(dataURL, function(data) {
-        console.log(data);
-        data.forEach(function(item) {
-            if (item.full_name == "alexmerren/alexmerren") {}
-            else {
-                let object = {
-                    'name' : item.name,
-                    'description' : item.description,
-                    'url' : item.url,
-                    'last_update' : item.pushed_at,
-                    'language' : item.language
-                };
-
-                cleanedData.push(object);
-            }
-        });
-    });
-
-    return cleanedData;
 }
