13b4f66cf3306ea7cdd386546bc72b904bb67334
Alex Merren <alexandermerren@gmail.com>
2021-04-18 15:31:11 +0100

    Completed projects functions

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,101 +1,115 @@
-// Function to be executed when website is loaded
 $(document).ready(function () {
-    // Getting repos from Github when requested
-    // let data = cleanRepoData();
-
     // Making the arrow bounce up and down at the bottom of the home div
-    setInterval(function() {
-        $("#title__down-arrow").show();
-        // setInterval(function() {
-        //     $("#down-arrow").effect("bounce", { times: 3, distance: 15 }, "slow");
-        // }, 1000);
+    setInterval(function () {
+        $('#title__down-arrow').show();
+        setInterval(function () {
+            $('#title__down-arrow').effect(
+                'bounce',
+                { times: 3, distance: 15 },
+                'slow'
+            );
+        }, 1000);
     }, 5000);
 
-
-    // Checking and setting color theme    
+    // Checking and setting color theme
     if (
-        localStorage.getItem("color-mode") === "dark" ||
-        (window.matchMedia("(prefers-color-scheme: dark)").matches &&
-            !localStorage.getItem("color-mode"))
+        localStorage.getItem('color-mode') === 'dark' ||
+        (window.matchMedia('(prefers-color-scheme: dark)').matches &&
+            !localStorage.getItem('color-mode'))
     ) {
-        $("html").attr("color-mode", "dark");
+        $('html').attr('color-mode', 'dark');
     }
 
     // Whenever scrolling, use the onScroll function
-    $(document).on("scroll", onScroll);
+    $(document).on('scroll', onScroll);
 
     // Whenever clicking on a link that points inside the web page,
     // use this custom function.
-    $("a[href^='#']").on("click", function (e) {
+    $("a[href^='#']").on('click', function (e) {
         e.preventDefault();
-        $(document).off("scroll");
+        $(document).off('scroll');
 
-        $("a").each(function () {
-            $(this).removeClass("active");
+        $('nav a').slice(0).each(function () {
+            $(this).removeClass('active');
         });
-        $(this).addClass("active");
+        $(this).addClass('active');
 
-        var target = this.hash;
+        let target = this.hash;
         $target = $(target);
 
-        $("html, body")
+        $('html, body')
             .stop()
-            .animate({ scrollTop: $target.offset().top }, 300, function () {
+            .animate({ scrollTop: $target.offset().top }, 200, function () {
                 window.location.has = target;
-                $(document).on("scroll", onScroll);
+                $(document).on('scroll', onScroll);
             });
     });
 });
 
 // Function to change the highlighted section in navbar
 function onScroll() {
-    var scrollPos = $(document).scrollTop() + $(window).height() / 2;
+    let currentLink, refElement, scrollPos;
+    scrollPos = $(document).scrollTop() + $(window).height() / 2;
 
-    $(".nav a")
-        .slice(1, 5)
+    $('.nav a')
+        .slice(1)
         .each(function () {
-            var currentLink = $(this);
-            var refElement = $(currentLink.attr("href"));
-            if (scrollPos >= refElement.position().top) {
-                $(".nav a").removeClass("active");
-                currentLink.addClass("active");
+            currentLink = $(this);
+            refElement = $(currentLink.attr('href'));
+            if (
+                refElement.position().top <= scrollPos &&
+                currentLink.offset().top + refElement.height() > scrollPos
+            ) {
+                $('.nav a').removeClass('active');
+                currentLink.addClass('active');
             } else {
-                currentLink.removeClass("active");
+                currentLink.removeClass('active');
             }
         });
 }
 
-$(document).ready( function() {
-    updateTable();
-});
-
 function updateTable() {
-    let dataURL = "https://api.github.com/users/alexmerren/repos"
-    let name, description, last_updated, url, language;
+    let iter, entry, name, description, last_updated, url, language;
+    let dataURL = 'https://api.github.com/users/alexmerren/repos';
+    let $table = $('.repos');
+    $table.empty();
 
-    $.getJSON(dataURL, function(data) {
-        for (let iter in data) {
-            let entry = data[iter];
-            if (entry.name == "alexmerren") {}
+    $table.append('<thead class="repos__title"><th>Name</th><th>Description</th><th>Language</th><th>Last Update</th></thead>');
+    $table.append('<tbody>');
+    $.getJSON(dataURL, function (data) {
+        entry = data[0];
+
+        for (iter in data) {
+            entry = data[iter];
+            if (entry.name === 'alexmerren') {} 
             else {
                 name = entry.name;
                 description = entry.description;
-                last_updated = entry.pushed_at;
+                last_updated = formatDate(entry.pushed_at);
                 language = entry.language;
                 url = entry.html_url;
-                //console.log(`${name}, ${description}, ${last_updated}, ${language}, ${url}`)
+                $('.repos >tbody').append(`<tr><td><a href="${url}">${name}</a></td><td>${description}</td><td>${language}</td><td>${last_updated}</td></tr>`);
             }
         }
     });
-    return cleanedData;
 }
 
-// Function to toggle dark mode
+// Function to toggle dark mode.
 function toggleTheme() {
-    if ($("html").attr("color-mode") == "light") {
-        $("html").attr("color-mode", "dark");
+    if ($('html').attr('color-mode') == 'light') {
+        $('html').attr('color-mode', 'dark');
     } else {
-        $("html").attr("color-mode", "light");
+        $('html').attr('color-mode', 'light');
     }
-    localStorage.setItem("color-mode", $("html").attr("color-mode"));
+    localStorage.setItem('color-mode', $('html').attr('color-mode'));
+}
+
+// Function to format date from github repo JSON.
+function formatDate(date) {
+    let day, month, year, shortDate;
+    year = date.slice(0,4);
+    month = date.slice(5,7);
+    day = date.slice(8,10);
+    shortDate = day + '/' + month + '/' + year;
+    return shortDate;
 }
