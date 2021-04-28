a5d0971929e11f3df19be787a9668196a4c48b34
Alex Merren <alexandermerren@gmail.com>
2021-04-28 02:33:34 +0100

    Added https

--- app.js
+++ app.js
@@ -1,18 +1,47 @@
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
 const express = require('express');
 const url = require('url');
 const app = express();
-const PORT = 80;
+
+//const privateKey = fs.readFileSync('', 'UTF-8');
+//const certificate = fs.readFileSync('', 'UTF-8');
+//const ca = fs.readFileSync('', 'UTF-8');
+//const credentials = {
+//    key: privateKey,
+//    cert: certificate,
+//    ca: ca
+//}
+
+// Create two server, one http and another https.
+//const httpServer = http.createServer(app);
+//const httpsServer = https.createServer(credentials, app);
 
 // Set the images/stylesheets to be in /static
 app.use(express.static(__dirname + '/static'));
+
+// If there is a https connection available, then redirect.
+//app.use((req, res, next) => {
+//    req.secure ? next() : res.redirect('https://' + req.headers.host + req.url);
+//});
 
 // Tell the server to render ejs files
 app.set('trust proxy', true);
 app.set('view engine', 'ejs');
 
-// Opens up the server to the port specified above 
-app.listen(PORT, function () {
-    console.log(`An app listening on port ${PORT}`);
+// Start the servers.
+//httpServer.listen(80, () => {
+//    console.log('HTTP server running on port 80');
+//});
+
+//httpsServer.listen(443, () => {
+//    console.log('HTTP server running on port 443');
+//});
+
+// Opens a server at port 8080 (testing only).
+app.listen(8080, function () {
+    console.log('An app listening on port 8080');
 });
 
 // Function to send homepage when request is made.
